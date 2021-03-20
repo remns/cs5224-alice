3aa0be98f8ab2d47a675ddb89a14e45f73feb650
Nic <remnants@outlook.com>
2021-03-20 15:51:01 +0800

    add apis /juniorcolleges & /polytechnics

---
+++ backend/src/handlers/get-polytechnic.js
@@ -0,0 +1,22 @@
+const dataFile = require("./../../data/polytechnics.json");
+
+exports.getPolytechnicHandler = async (event) => {
+    const { httpMethod, path } = event;
+    if (httpMethod !== 'GET') {
+        throw new Error(`${httpMethod} method not allowed.`);
+    }
+    // All log statements are written to CloudWatch by default. For more information, see
+    // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
+    console.log('received:', JSON.stringify(event));
+
+    const response = {
+        statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Origin" : "*"
+        },
+        body: JSON.stringify(dataFile)
+    };
+
+    console.log(`response from: ${path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+};
