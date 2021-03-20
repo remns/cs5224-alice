1542100f3c2ebbecc4663ea3ac68ecd8387f013d
Tan_Jun_Bang <tan_jun_bang@hotmail.com>
2021-03-20 22:24:44 +0800

    Consolidate all API calls for frontend.

---
+++ frontend/src/api/API.js
@@ -0,0 +1,31 @@
+import {SERVER_URL} from './serverConfig'
+
+function getAllUniversity() {
+  const fetchPromise = fetch(SERVER_URL + "/universities")
+  return fetchPromise
+}
+
+function getAllInterest() {
+  const fetchPromise = fetch(SERVER_URL + "/interests")
+  return fetchPromise
+}
+
+function getAllCourse() {
+  const fetchPromise = fetch(SERVER_URL + "/courses")
+  return fetchPromise
+}
+
+function getAllJC() {
+  const fetchPromise = fetch(SERVER_URL + "/juniorcolleges")
+  return fetchPromise
+}
+
+function getAllPoly() {
+  const fetchPromise = fetch(SERVER_URL + "/polytechnics")
+  return fetchPromise
+}
+
+
+export {
+  getAllUniversity,
+}
---
+++ frontend/src/api/serverConfig.js
@@ -0,0 +1,5 @@
+const SERVER_URL = 'https://3jqms9dxp6.execute-api.ap-southeast-1.amazonaws.com/Prod'
+
+export {
+  SERVER_URL
+}
